c8e12c49b0d92423e83d888ac24de0aa24524ab6
byronskout <vyskoutaris@hotmail.com>
2019-11-27 10:56:55 +0000

    Products migration

--- db/migrate/20191127104816_create_products.rb
+++ db/migrate/20191127104816_create_products.rb
@@ -4,7 +4,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
       t.string :name
       t.string :description
       t.string :brand
-      t.decimal :price
+      t.decimal :price, precision: 5, scale: 2, default: 0
       t.integer :stock_level
 
       t.timestamps
