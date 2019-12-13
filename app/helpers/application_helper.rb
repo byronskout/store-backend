b2a50a664918489e839cf869a9bca16a276f4172
byronskout <vyskoutaris@hotmail.com>
2019-12-13 12:20:52 +0000

    ApplicationHelper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,11 @@
 module ApplicationHelper
-    def basket_count_visible
-      if @basket.basket_items.count > 0
-        return "<span>#{@basket.basket_items.count}</span>".html_safe
-      end
-    end
-  
-    def basket_has_products
-      return @basket.basket_items.count > 0
+  def basket_count_visible
+    if @basket.basket_items.count > 0
+      return "<span>#{@basket.basket_items.count}</span>".html_safe
     end
   end
-  
+
+  def basket_has_products
+    return @basket.basket_items.count > 0
+  end
+end
