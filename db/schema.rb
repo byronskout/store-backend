edc59439aa91bc8b7b6ca7f20329b1df3e269a73
byronskout <vyskoutaris@hotmail.com>
2019-12-13 12:25:26 +0000

    Basket & basket_item models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_110526) do
+ActiveRecord::Schema.define(version: 2019_12_13_122433) do
+
+  create_table "basket_items", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "baskets", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
